9416c3645067589f4f01b951d78837a63f4d064b
Luiz Skora <luiz.skora1@gmail.com>
2026-02-01 14:40:57 -0600

    Versão 01 Alpha (produção) do Admin Bula Digital

--- config/initializers/openai.rb
+++
@@ -1,6 +0,0 @@
-require 'openai'
-
-OpenAI.configure do |config|
-  config.access_token = 'sk-ef70c5f8e570414fb4a9e764caf7a373'
-  config.uri_base = "https://api.deepseek.com/v1/" # Endpoint específico do DeepSeek
-end
